53263b9b9337997cc92a3f50d9ca32c6fa288272
Ivana Stojadinović <ivanastojadinovic001@gmail.com>
2024-02-22 13:55:17 +0100

    add git repo

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -13,15 +13,40 @@ const Portfolio = () => {
         <h1 className='Ptitle'>My Portfolio</h1>
         <h2 className='Psubtitle'>JavaScript projects</h2>
         <div className="card-container">
+          <div className='card'>
             <a href='https://ivana-01.github.io/clock.github.io/' target='_blank' className='project'><img src={ clockImg } alt='' className='projectImg'/></a>
+            <a href='https://github.com/Ivana-01/clock.github.io' target='_blank' className='projectRepo'>GitHub Repo</a>
+          </div>
+          <div className='card'>
             <a href='https://ivana-01.github.io/stopwatch.github.io/' target='_blank' className='project'><img src={ stopwatchJs } alt='' className='projectImg'/></a>
+            <a href='https://github.com/Ivana-01/stopwatch.github.io' target='_blank' className='projectRepo'>GitHub Repo</a>
+          </div>
+          <div className='card'>
             <a href='https://ivana-01.github.io/weather-app-js.github.io/' target='_blank' className='project'><img src={ weatherJs } alt='' className='projectImg'/></a>
+            <a href='https://github.com/Ivana-01/weather-app-js.github.io' target='_blank' className='projectRepo'>GitHub Repo</a>
+          </div>
         </div>
         <h2 className='Psubtitle'>React projects</h2>
         <div className="card-container">
+        <div className='card'>
             <a href='https://ivana-01.github.io/weather-app.github.io/' target='_blank' className='project'><img src={ weatherR } alt='' className='projectImg'/></a>
+            <a href='https://github.com/Ivana-01/weather-app.github.io' target='_blank' className='projectRepo'>GitHub Repo</a>
+          </div>
+          <div className='card'>
             <a href='https://ivana-01.github.io/sasorisstore.github.io/' target='_blank' className='project'><img src={ sasori } alt='' className='projectImg'/></a>
+            <a href='https://github.com/Ivana-01/sasorisstore.github.io' target='_blank' className='projectRepo'>GitHub Repo</a>
+          </div>
+          <div className='card'>
             <a href='https://ivana-01.github.io/movie-app.github.io/' target='_blank' className='project'><img src={ movieR } alt='' className='projectImg'/></a>
+            <a href='https://github.com/Ivana-01/movie-app.github.io' target='_blank' className='projectRepo'>GitHub Repo</a>
+          </div>
+        </div>
+        <h2 className='Psubtitle'>MERN stack projects</h2>
+        <div className="card-container">
+          <div className='mernProject'>
+            <h3>Bookstore project:</h3>
+            <a href='https://github.com/Ivana-01/mernBookstore.github.io/' target='_blank' className='projectRepo'>GitHub Repo</a>
+          </div>
         </div>
     </section>
   )
